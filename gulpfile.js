cc015e95ff043241fc2842b5b12b6b9e7fb50f34
Paul Wylie <paul@Pauls-MacBook-Pro.local>
2017-11-02 15:13:18 +0000

    added two plugins Gulp-concat and Gulp-Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass');
 let watch = require('gulp-watch');
-let gulpSequence = require('gulp-sequence')
+let gulpSequence = require('gulp-sequence');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let pump = require('pump');
 
 gulp.task('sass', function () {
     var stream = gulp.src('./scss/styles.scss')
@@ -24,6 +27,28 @@ gulp.task('styles', function(callback){
 	gulpSequence('sass', 'minify-css')(callback)
 });
 
+gulp.task('scripts', function() {
+  return gulp.src(['./js/jquery-3.2.1.slim.js', './js/popper.js', './js/bootstrap.js',])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./js-dist/'));
+});
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./js-dist/*.js'),
+        uglify(),
+        gulp.dest('./js-dist/')
+    ],
+    cb
+  );
+});
+
+gulp.task('js', function(callback){
+    gulpSequence('scripts', 'compress')(callback)
+});
+
 gulp.task('watch', function () {
 	gulp.watch('./scss/*.scss', ['styles']);
+    gulp.watch('./js/*.js', ['js']);
 });
+
